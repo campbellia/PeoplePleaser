684b95c9645e6df9ea77f5772417131d85916599
Angelia Campbell <ali.belclamp@gmail.com>
2019-09-30 11:44:59 -0400

    add post route to api

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const controllers = require('../database/controllers.js');
 
+app.use(bodyParser.json());
 app.use('/', express.static('public/'));
+
+app.post('/polls', (req, res) => {
+  console.log('POST REQUEST>>>>', req.body)
+});
 
 app.listen(port, () => {
   console.log('Server is listening at port ', port);
