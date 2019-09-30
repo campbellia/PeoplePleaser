d7c51858365b85b0f7d32d75e47479d84a5d5534
Angelia Campbell <ali.belclamp@gmail.com>
2019-09-30 11:43:33 -0400

    add post route for creating a new poll

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,22 +1,38 @@
 import React, { useState } from 'react';
 import CreatePollForm from './CreatePollForm.jsx';
 import Poll from './Poll.jsx';
+import axios from 'axios';
 
-const App = ({id}) => {
+const App = ({pollId}) => {
 
-  const [poll, setPoll] = useState(null);
+  const [poll, setPoll] = useState(pollId ? pollId : null);
+  const [results, setResults] = useState(null);
 
   const onFormSubmit = (name, options) => {
     var pollInfo = {
       name: name,
-      options: options
+      options: options,
+      totalVotes: 0
     }
-    setPoll(pollInfo);
+    console.log("in on formsubmit", pollInfo);
+    axios.post('/polls', pollInfo)
+      .then(res => {
+        console.log('PollId:', res);
+        //setPoll(res);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  const handleSubmitVotes = (results) => {
+    //submit the results to the db
+    //display results
   }
 
   const getComponents = () => {
     if (poll) {
-      return <Poll options={poll}/>;
+      return <Poll handleSubmitVotes={handleSubmitVotes} poll={poll}/>;
     } else {
       return <CreatePollForm onFormSubmit={onFormSubmit}/>;
     }
--- client/src/components/Poll.jsx
+++ client/src/components/Poll.jsx
@@ -1,7 +1,49 @@
 import React, { useState } from 'react';
+import Slider from '@material-ui/core/Slider';
 
 const Poll = (props) => {
-  return <div>Poll!</div>;
+  var neutralResults = {};
+  props.poll.options.forEach(option => {
+    neutralResults[option] = 0;
+  });
+
+  const [results, setResults] = useState(neutralResults);
+
+  const onSliderChange = (e, val) => {
+    var newResults = {};
+    Object.assign(newResults, results);
+    newResults[e.target.id] = val;
+    setResults(newResults);
+  }
+
+  const handleSubmitVotes = () => {
+    props.handleSubmitVotes(results);
+  }
+
+  return (
+    <div className="poll">
+      <h1>{props.poll.name}</h1>
+      <form onSubmit={handleSubmitVotes}>
+        {props.poll.options.map(option => {
+          return (
+          <div className="option">
+            <h4>{option}</h4>
+            <Slider
+              defaultValue={0}
+              step={1}
+              marks
+              min={-5}
+              max={5}
+              id={option}
+              onChange={onSliderChange}
+            />
+          </div>
+          )
+        })}
+        <input type="submit" value="Submit Votes"></input>
+      </form>
+    </div>
+  );
 
 }
 
