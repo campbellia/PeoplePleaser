201e1e6478c98b71ca1a390ccf8c5e054cf8b265
Angelia Campbell <ali.belclamp@gmail.com>
2019-10-02 16:19:20 -0400

    add material ui theme provider

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,29 +1,29 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { withStyles } from '@material-ui/styles';
+import { createBrowserHistory } from "history"
 import CreatePollForm from './CreatePollForm.jsx';
 import Poll from './Poll.jsx';
 import Results from './Results.jsx';
 import Navbar from './Navbar.jsx';
-import { createBrowserHistory } from "history"
+import { createMuiTheme } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
 
-const defaultTheme = createMuiTheme();
+const theme = createMuiTheme();
 
 const history = createBrowserHistory();
 
 const App = () => {
 
   return (
-    <MuiThemeProvider theme={defaultTheme}>
+    <ThemeProvider theme={theme}>
       <Router history={history}>
-        <Route path="/" component={Navbar}/>
-          <Route exact={true} path="/" component={CreatePollForm}/>
-          <Route path="/polls/:pollId/vote" component={Poll}/>
-          <Route path="/polls/:pollId/results" component={Results}/>
+        <Navbar/>
+        <Route exact={true} path="/" component={CreatePollForm}/>
+        <Route path="/polls/:pollId/vote" component={Poll}/>
+        <Route path="/polls/:pollId/results" component={Results}/>
       </Router>
-    </MuiThemeProvider>
+    </ThemeProvider>
   );
 
 }
