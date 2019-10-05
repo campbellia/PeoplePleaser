4dcc5923d876246f477da3e668492683f70c1206
Angelia Campbell <ali.belclamp@gmail.com>
2019-10-04 23:27:51 -0400

    add dotenv

--- database/index.js
+++ database/index.js
@@ -1,15 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const config = require("../firebaseConfig.js");
 const firebase = require("firebase");
 require("firebase/firestore");
 
 // Initialize Firebase
-firebase.initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
-});
+firebase.initializeApp(config);
 
 module.exports = firebase.firestore();
