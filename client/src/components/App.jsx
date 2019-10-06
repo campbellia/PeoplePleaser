7b4b072bd2484dbc1fe8671babcbd6e3ba43b7a1
Angelia Campbell <ali.belclamp@gmail.com>
2019-10-05 20:04:48 -0400

    utilize codesplitting, dynamic imports

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history'
-import CreatePollForm from './CreatePollForm.jsx';
-import Poll from './Poll.jsx';
-import Results from './Results.jsx';
+// import CreatePollForm from './CreatePollForm.jsx';
+// import Poll from './Poll.jsx';
+// import Results from './Results.jsx';
 import Navbar from './Navbar.jsx';
 import { createMuiTheme } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
@@ -12,10 +12,10 @@ import { ThemeProvider } from '@material-ui/styles';
 const theme = createMuiTheme();
 const history = createBrowserHistory();
 
-// import asyncComponent from './AsyncComponent.jsx';
-// const AsyncCreatePollForm = asyncComponent(() => import('./CreatePollForm.jsx'));
-// const AsyncPoll = asyncComponent(() => import('./Poll.jsx'));
-// const AsyncResults = asyncComponent(() => import('./Results.jsx'));
+import asyncComponent from './AsyncComponent.jsx';
+const AsyncCreatePollForm = asyncComponent(() => import('./CreatePollForm.jsx'));
+const AsyncPoll = asyncComponent(() => import('./Poll.jsx'));
+const AsyncResults = asyncComponent(() => import('./Results.jsx'));
 
 const App = () => {
 
@@ -23,9 +23,9 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <Router history={history}>
           <Route path="/" component={Navbar}/>
-          <Route exact={true} path="/" component={CreatePollForm}/>
-          <Route path="/polls/:pollId/vote" component={Poll}/>
-          <Route path="/polls/:pollId/results" component={Results}/>
+          <Route exact={true} path="/" component={AsyncCreatePollForm}/>
+          <Route path="/polls/:pollId/vote" component={AsyncPoll}/>
+          <Route path="/polls/:pollId/results" component={AsyncResults}/>
       </Router>
     </ThemeProvider>
   );
